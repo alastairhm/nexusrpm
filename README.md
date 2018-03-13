cbc0df075ef41862abadaa02c1996e29d59646e2
Alastair Montgomery <amontgomery@williamhill.co.uk>
2018-03-13 09:24:23 +0000

    Optionally create a DEB package

--- README.md
+++ README.md
@@ -4,7 +4,19 @@ Create a RPM package for installing Sonatype Nexus 3 Repository Manager
 Run with
 
 **Latest released version**
+
 `docker run -it --name rpmbuilder alastairhm/nexusrpm:latest`
 
 **Specify a version**
+
 `docker run -it -e DOWNLOAD_VERSION=3.8.0-02 --name rpmbuilder alastairhm/nexusrpm:latest`
+
+**Build a deb package**
+
+`docker run -it -e TARGET_PACKAGE=deb --name rpmbuilder alastairhm/nexusrpm:latest`
+
+**Parameters**
+
+DOWNLOAD_VERSION : optional, specify the version of Nexus to download, defaults to "latest" release.
+
+TARGET_PACKAGE : optional, specify package type "rpm" or "deb", defaults to "rpm".
--- create_repo.sh
+++ create_repo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DVER=${DOWNLOAD_VERSION:=latest}
-echo "Downloading ${DVER} Nexus 3 build for Unix"
+TARGET=${TARGET_PACKAGE:=rpm}
+echo "Downloading ${DVER} Nexus 3 build for Unix, target package type ${TARGET}"
 if [[ $DVER == 'latest' ]]
 then
 	wget --quiet https://download.sonatype.com/nexus/3/latest-unix.tar.gz
@@ -12,7 +13,7 @@ tar xf *-unix.tar.gz && rm -f *-unix.tar.gz
 VERSION=$(ls -d nexus-* |cut -d '-' -f 2-)
 NAME=${VERSION/-/.}
 echo "Creating RPM for ${VERSION}"
-/usr/local/bin/fpm -s dir -t rpm -n nexus -v $NAME nexus-$VERSION/=/var/lib/nexus/nexus-$VERSION sonatype-work/=/var/lib/nexus/sonatype-work
-echo "Copy RPM out of container with command 'docker cp rpmbuilder:/build/nexus-${NAME}-1.x86_64.rpm ./'"
-
+/usr/local/bin/fpm -s dir -t $TARGET -n nexus -v $NAME nexus-$VERSION/=/var/lib/nexus/nexus-$VERSION sonatype-work/=/var/lib/nexus/sonatype-work
+FILE=$(ls nexus*$TARGET)
+echo "Copy RPM out of container with command 'docker cp rpmbuilder:/build/${FILE} ./'"
 
